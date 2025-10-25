c5ff450301709d7d42af394122964174592ddaa0
Toan Pham <ductoan.uit@gmail.com>
2025-10-25 01:08:15 +0000

    init repo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GoSocialX
+- Go 1.22
+- Docker
+- Postgres in docker
+- swagger for docs
+- golang migrate for migrations
---
+++ cmd/api/api.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+	return r
+	//posts
+
+	//users
+
+	//auth
+
+}
+
+func (app *application) run(mux *chi.Mux) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+	log.Printf("server has start at %s", app.config.addr)
+	return srv.ListenAndServe()
+}
